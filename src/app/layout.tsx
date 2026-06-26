e8ae3e766d9b8ddc89ea732a09b0c01bf5f1c85c
VladikNV250 <vladiknemichev@gmail.com>
2026-06-26 12:58:22 +0300

    chore(CYB-2): upload logo images and font to the project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,56 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import localFont from 'next/font/local';
 import './globals.css';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+const sfPro = localFont({
+  src: [
+    {
+      path: '../shared/assets/fonts/SFPRODISPLAYTHINITALIC.otf',
+      weight: '100',
+      style: 'italic',
+    },
+    {
+      path: '../shared/assets/fonts/SFPRODISPLAYULTRALIGHTITALIC.otf',
+      weight: '200',
+      style: 'italic',
+    },
+    {
+      path: '../shared/assets/fonts/SFPRODISPLAYLIGHTITALIC.otf',
+      weight: '300',
+      style: 'italic',
+    },
+    {
+      path: '../shared/assets/fonts/SFPRODISPLAYREGULAR.otf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../shared/assets/fonts/SFPRODISPLAYMEDIUM.otf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../shared/assets/fonts/SFPRODISPLAYSEMIBOLDITALIC.otf',
+      weight: '600',
+      style: 'italic',
+    },
+    {
+      path: '../shared/assets/fonts/SFPRODISPLAYBOLD.otf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../shared/assets/fonts/SFPRODISPLAYHEAVYITALIC.otf',
+      weight: '800',
+      style: 'italic',
+    },
+    {
+      path: '../shared/assets/fonts/SFPRODISPLAYBLACKITALIC.otf',
+      weight: '900',
+      style: 'italic',
+    },
+  ],
+  variable: '--font-sf-pro',
 });
 
 export const metadata: Metadata = {
@@ -23,11 +64,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en" className="h-full antialiased">
+      <body
+        className={`min-h-full flex flex-col ${sfPro.className}`}
+        suppressHydrationWarning
+      >
+        {children}
+      </body>
     </html>
   );
 }
