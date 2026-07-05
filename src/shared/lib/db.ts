6f578e0eac2b14d3ce890ea365c05e09152a74ba
VladikNV250 <vladiknemichev@gmail.com>
2026-07-05 17:05:21 +0300

    refactor(CYB-3): error handler for postgres client

--- src/shared/lib/db.ts
+++ src/shared/lib/db.ts
@@ -3,6 +3,10 @@ import { PrismaPg } from '@prisma/adapter-pg';
 import { Pool } from 'pg';
 
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+pool.on('error', (err) => {
+  console.error('Unexpected error on idle Postgres client', err);
+});
+
 const adapter = new PrismaPg(pool);
 
 const globalForPrisma = globalThis as unknown as {
