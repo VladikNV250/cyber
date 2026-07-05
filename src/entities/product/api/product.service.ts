c1aa955f6cfbd1c08daf4a772f1b514a89e56267
VladikNV250 <vladiknemichev@gmail.com>
2026-07-05 17:26:36 +0300

    refactor(CYB-3): return error if the product variant appears in the order history

--- src/entities/product/api/product.service.ts
+++ src/entities/product/api/product.service.ts
@@ -208,13 +208,25 @@ export async function updateProductVariant(
 }
 
 export async function deleteProductVariant(id: string) {
-  return prisma.$transaction(async (tx) => {
-    const variant = await tx.productVariant.delete({
-      where: { id },
+  try {
+    return await prisma.$transaction(async (tx) => {
+      const variant = await tx.productVariant.delete({
+        where: { id },
+      });
+      await updateProductMinPrice(variant.productId, tx);
+      return variant;
     });
-    await updateProductMinPrice(variant.productId, tx);
-    return variant;
-  });
+  } catch (error) {
+    if (
+      error instanceof Prisma.PrismaClientKnownRequestError &&
+      error.code === 'P2003'
+    ) {
+      throw new Error(
+        'This variant appears in the order history and cannot be deleted.',
+      );
+    }
+    throw error;
+  }
 }
 
 async function updateProductMinPrice(
